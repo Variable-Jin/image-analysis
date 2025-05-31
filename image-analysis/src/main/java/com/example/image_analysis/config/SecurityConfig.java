e3e2b44bc2ee6ec83fadfb7451cb2395ea939817
Variable-Jin <tnwls010223@gmail.com>
2025-05-31 21:41:42 +0900

    user 인증 페이지 명시 및 JWT 인증 필터 적용

--- image-analysis/src/main/java/com/example/image_analysis/config/SecurityConfig.java
+++ image-analysis/src/main/java/com/example/image_analysis/config/SecurityConfig.java
@@ -1,31 +1,50 @@
 package com.example.image_analysis.config;
 
+import com.example.image_analysis.JwtTokenProvider;
+import com.example.image_analysis.filter.JwtAuthenticationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
+
+    @Bean
+    @Order(2)
+    public SecurityFilterChain securityFilterChain
+            (HttpSecurity http, JwtTokenProvider jwtTokenProvider) throws Exception {
         http
                 .authorizeHttpRequests( (authz) -> authz
-                        // 인증이 필요한 API만 명시 예정
+                        .requestMatchers("/api/v1/users/me", "/api/v1/users/update", "/api/v1/users/delete")
+                        .authenticated()
                         .anyRequest().permitAll()
+                        // 인증이 필요한 API만 명시 예정
                 )
-                .formLogin(form -> form
-                        .loginPage("/login")
-                        .defaultSuccessUrl("/")
-                        .permitAll()
-                )
+                .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class)
+//                .formLogin(form -> form
+//                        .loginPage("/login")
+//                        .defaultSuccessUrl("/")
+//                        .permitAll()
+//                )
                 .logout(logout -> logout
                         .logoutSuccessUrl("/")
                         .permitAll()
                 )
+                .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider),
+                        UsernamePasswordAuthenticationFilter.class)
+
                 .csrf(csrf -> csrf.disable());
 
         return http.build();
